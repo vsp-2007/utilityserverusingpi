9829fb6e76d1e6876f92286b0ca023eb5248cb3a
vsp-2007 <vsp-2007@users.noreply.github.com>
2026-03-09 01:47:11 +0530

    Fix bash variable expansion syntax error in Nginx config

--- scripts/09-reverse-proxy.sh
+++ scripts/09-reverse-proxy.sh
@@ -105,11 +105,11 @@ server {
 
     location / {
         proxy_pass http://localhost:9091;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
     }
 }
